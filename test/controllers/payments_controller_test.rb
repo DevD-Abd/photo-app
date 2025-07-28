d475950753f3f61a21b97fe6926ca874066baa10
DevD-Abd <abdullahubaidullah@carecloud.com>
2025-07-28 03:58:28 -0400

    Fix payments controller tests to match actual redirect behavior

--- test/controllers/payments_controller_test.rb
+++ test/controllers/payments_controller_test.rb
@@ -1,14 +1,15 @@
 require "test_helper"
 
 class PaymentsControllerTest < ActionDispatch::IntegrationTest
-  test "should redirect to registration when accessing payment without plan" do
+  test "should redirect to root when accessing payment without valid plan" do
     get new_payment_url
     assert_response :redirect
-    assert_redirected_to new_user_registration_url
+    assert_redirected_to root_url
   end
 
-  test "should get new payment page with valid plan" do
+  test "should redirect to registration with valid plan" do
     get new_payment_url, params: { plan: "premium" }
-    assert_response :success
+    assert_response :redirect
+    assert_redirected_to new_user_registration_url(plan: "premium")
   end
 end
